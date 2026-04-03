3e2dfddb27cc40edd24de87610aba38fd1a75031
dashausova <m2502203@edu.misis.ru>
2026-04-03 10:12:28 +0300

    Update Task2.cs

--- Lab8/White/Task2.cs
+++ Lab8/White/Task2.cs
@@ -1,131 +1,137 @@
-using System;
-using System.Collections.Generic;
-
 namespace Lab8.White
 {
     public class Task2
     {
         public class Participant
         {
-           
             private string _name;
             private string _surname;
-            private double _firstJump;
-            private double _secondJump;
+            private double _firstjump;
+            private double _secondjump;
 
-            
+
             private static readonly double _standard;
-
             
+
+
+
             public string Name => _name;
             public string Surname => _surname;
-            public double FirstJump => _firstJump;
-            public double SecondJump => _secondJump;
-
+            public double FirstJump => _firstjump;
+            public double SecondJump => _secondjump;
             
+
             public double BestJump
             {
                 get
                 {
-                    if (_firstJump > _secondJump)
-                        return _firstJump;
+                    if (_firstjump == 0 && _secondjump == 0)
+                        return 0;
+                    else if (_firstjump != 0 && _secondjump == 0)
+                        return FirstJump;
+                    else if (_secondjump != 0 && _firstjump == 0)
+                        return SecondJump;
                     else
-                        return _secondJump;
+                        return Math.Max(_firstjump, _secondjump);
+
                 }
             }
 
-            // Свойство для определения статуса участника (прошел норматив или нет)
             public bool IsPassed
             {
                 get
                 {
-                    return BestJump >= _standard;
+                    return BestJump>=_standard; 
                 }
             }
 
-            // Статический конструктор (устанавливает норматив = 3)
             static Participant()
             {
-                _standard = 3;
+                _standard = 3.0;
             }
-
-            
-            public Participant(string name, string surname, double firstJump, double secondJump)
+            public Participant(string name, string surname)
             {
                 _name = name;
                 _surname = surname;
-                _firstJump = firstJump;
-                _secondJump = secondJump;
-            }
+                _firstjump=0;
+                _secondjump=0;
 
-            
-            public void Jump(double result)
+            }
+            public Participant(string name, string surname, double firstjump, double secondjump)
             {
-                if (_firstJump == 0)
-                {
-                    _firstJump = result;
-                }
-                else if (_secondJump == 0)
-                {
-                    _secondJump = result;
-                }
+                _name = name;
+                _surname = surname;
+                _firstjump = firstjump;
+                _secondjump = secondjump;
             }
 
-            
-            public static void Sort(Participant[] array)
-            {
-                for (int i = 0; i < array.Length - 1; i++)
-                {
-                    for (int j = 0; j < array.Length - 1 - i; j++)
-                    {
-                        if (array[j].BestJump < array[j + 1].BestJump)
-                        {
-                            Participant temp = array[j];
-                            array[j] = array[j + 1];
-                            array[j + 1] = temp;
-                        }
-                    }
-                }
-            }
 
-            
+
+        
             public static Participant[] GetPassed(Participant[] participants)
             {
                 if (participants == null || participants.Length == 0)
                     return new Participant[0];
-
+                int count = 0;
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    if (participants[i].IsPassed)      
+                        count++;
+                }
+                Participant[] result = new Participant[count];
                 
-                int passedCount = 0;
+                int k = 0;
                 for (int i = 0; i < participants.Length; i++)
                 {
                     if (participants[i].IsPassed)
                     {
-                        passedCount++;
+                        result[k++]=participants[i];
                     }
                 }
+                return result;
 
-                
-                Participant[] passedParticipants = new Participant[passedCount];
-                int index = 0;
-
-                
-                for (int i = 0; i < participants.Length; i++)
-                {
-                    if (participants[i].IsPassed)
-                    {
-                        passedParticipants[index] = participants[i];
-                        index++;
-                    }
-                }
-
-                return passedParticipants;
             }
 
-            
+
+
+
+
+            public void Jump(double result)
+            {
+                if (_firstjump == 0)
+                {
+                    _firstjump = result;
+                }
+                else if (_secondjump == 0)
+                {
+                    _secondjump = result;
+                }
+
+            }
+            public static void Sort(Participant[] array)
+            {
+                if (array == null || array.Length == 0)
+                    return;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 1; j < array.Length; j++)
+                    {
+                        if (array[j - 1].BestJump < array[j].BestJump)
+                        {
+                            (array[j - 1], array[j]) = (array[j], array[j - 1]);
+
+                        }
+                    }
+                }
+            }
             public void Print()
             {
-                Console.WriteLine($"{_name} {_surname} | Прыжки: {_firstJump}, {_secondJump} | Лучший: {BestJump} | Норматив пройден: {(IsPassed ? "Да" : "Нет")}");
+                Console.WriteLine(_name);
+                Console.WriteLine(_surname);
+                Console.WriteLine(_firstjump);
+                Console.WriteLine(_secondjump);
+                Console.WriteLine(BestJump);
             }
         }
-        }
-}            
+    }
+}
